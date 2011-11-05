4a0fd2894188d51d523684d66903046206d0cb71
Andreas Isaak <andy.jared@googlemail.com>
2011-11-05 20:37:28 +0100

    Add extension check

---
+++ system/modules/slideitmoo/slideItMoo.php
@@ -0,0 +1,81 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  MEN AT WORK 2011 
+ * @package    slideItMoo
+ * @license    GNU/LGPL 
+ * @filesource
+ */
+
+/**
+ * Class slideItMoo 
+ *
+ * @copyright  MEN AT WORK 2011 
+ * @package    Backend
+ */
+class slideItMoo extends Backend
+{
+
+    /**
+     * Check the required extensions and files
+     * @param string $strContent
+     * @param string $strTemplate
+     * @return string
+     */
+    public function checkExtensions($strContent, $strTemplate)
+    {
+        if ($strTemplate == 'be_main')
+        {
+            if (!is_array($_SESSION["TL_INFO"]))
+            {
+                $_SESSION["TL_INFO"] = array();
+            }
+
+            // required extensions
+            $arrRequiredExtensions = array(
+                'slideItMooFramework'
+            );
+
+            // check for required extensions
+            foreach ($arrRequiredExtensions as $val)
+            {
+                if (!in_array($val, $this->Config->getActiveModules()))
+                {
+                    $_SESSION["TL_INFO"] = array_merge($_SESSION["TL_INFO"], array($val => 'Please install the required extension <strong>' . $val . '</strong>'));
+                }
+                else
+                {
+                    if (is_array($_SESSION["TL_INFO"]) && key_exists($val, $_SESSION["TL_INFO"]))
+                    {
+                        unset($_SESSION["TL_INFO"][$val]);
+                    }
+                }
+            }
+        }
+
+        return $strContent;
+    }
+
+}
+
+?>
