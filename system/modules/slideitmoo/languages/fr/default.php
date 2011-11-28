94d6e489c651b95b64c2adf5dae670f959e35f32
Andreas Isaak <andy.jared@googlemail.com>
2011-11-28 12:31:24 +0100

    Add france translation

---
+++ system/modules/slideitmoo/languages/fr/default.php
@@ -0,0 +1,38 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2011 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  MEN AT WORK 2011 
+ * @package    Language
+ * @license    GNU/LGPL 
+ * @filesource
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['CTE']['slideIt'] = 'SlideItMoo';
+$GLOBALS['TL_LANG']['CTE']['slideItStart'] = array('SlideItMoo début','Différentes configurations sont appliquées à l\'élément de début du diaporama.');
+$GLOBALS['TL_LANG']['CTE']['slideItEnd'] = array('SlideItMoo fin','Aucune modification du diaporama ne peut être effectuée ici. <br />!!! Si vous supprimez cet élément, l\'ensemble du diaporama sera supprimé !!!');
+
+?>
