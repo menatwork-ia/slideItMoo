1899f1838a50a82983658b358597cd14d95549dd
mediabakery <seb.tilch@gmx.de>
2012-08-08 09:34:22 +0200

    Fix bug missed itemDimensions
    when the itemDimensions are missed it will be calculated

--- system/modules/slideitmoo/html/js/slideitmoo_src.js
+++ system/modules/slideitmoo/html/js/slideitmoo_src.js
@@ -39,7 +39,31 @@ var ExtendedSlideItMoo = new Class({
         
         if($(this.options.containerId))
         {
-            var objSlider = new SlideItMoo(this.options.sliderAttr);
+            // Fix bug missed itemDimensions [START]
+            if (! this.options.sliderAttr.itemWidth)
+            {
+            	var elementItem = $(this.options.sliderAttr.elementScrolled).getElements(this.options.sliderAttr.itemsSelector)[0];
+            	var arrMargin = elementItem.getStyle('margin').split(' ');
+            	var strItemWidth = elementItem.getStyle('width');
+            	var strItemHeight = elementItem.getStyle('height');
+           
+            	if (strItemWidth.contains('px') && arrMargin[1].contains('px') && arrMargin[3].contains('px')) {
+            		var intWidth = strItemWidth.substring(0,strItemWidth.length-2);
+            		var intMarginLeft = arrMargin[3].substring(0,arrMargin[3].length-2);
+            		var intMarginRight = arrMargin[1].substring(0,arrMargin[1].length-2);
+            		this.options.sliderAttr.itemWidth = intWidth.toInt()+intMarginLeft.toInt()+intMarginRight.toInt()
+            	}
+            	
+				if (strItemHeight.contains('px') && arrMargin[0].contains('px') && arrMargin[2].contains('px')) {
+            		var intHeight = strItemWidth.substring(0,strItemWidth.length-2);
+            		var intMarginTop = arrMargin[0].substring(0,arrMargin[0].length-2);
+            		var intMarginBottom = arrMargin[2].substring(0,arrMargin[2].length-2);
+            		this.options.sliderAttr.itemHeight = intHeight.toInt()+intMarginTop.toInt()+intMarginBottom.toInt()
+            	}
+            }
+            // Fix bug missed itemDimensions [END]#
+           	
+           	var objSlider = new SlideItMoo(this.options.sliderAttr);
             var optSlider = objSlider.options;
             
             var sliderAttr = this.options.sliderAttr;
