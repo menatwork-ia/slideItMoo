dce47f9adea649979bbad9b46a70ae0224bbd4a6
Patrick Kahl <kahl.patrick@googlemail.com>
2013-07-10 15:44:44 +0200

    Add autoload for contao 3

---
+++ system/modules/slideitmoo/config/autoload.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * @copyright  MEN AT WORK 2013
+ * @package    slideitmoo
+ * @license    GNU/LGPL
+ * @filesource
+ */
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'slideItEnd'    => 'system/modules/slideitmoo/slideItEnd.php',
+	'slideItModule' => 'system/modules/slideitmoo/slideItModule.php',
+	'slideItMoo'    => 'system/modules/slideitmoo/slideItMoo.php',
+	'slideItHelper' => 'system/modules/slideitmoo/slideItHelper.php',
+	'slideItStart'  => 'system/modules/slideitmoo/slideItStart.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'mod_slideItMoo'  => 'system/modules/slideitmoo/templates',
+	'moo_slideItMoo'  => 'system/modules/slideitmoo/templates',
+	'ce_slideItEnd'   => 'system/modules/slideitmoo/templates',
+	'ce_slideItStart' => 'system/modules/slideitmoo/templates',
+));
